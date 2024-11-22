193b6e03fbb98c66bb587fc2b37f590288107376
wolfgang-99 <56457199+wolfgang-99@users.noreply.github.com>
2024-11-22 11:25:57 -0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def home():
+    return jsonify({"message": "Welcome to the secure API "}), 200
+
 @app.route('/check_payment', methods=['GET'])
 def check_payment():
     """Endpoint that returns the payment status."""
